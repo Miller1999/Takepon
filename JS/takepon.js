d9dc2b7a594d0477b1c905e04cb71aa78a5b4d2b
Miller1999 <millerjavierarias@gmail.com>
2023-01-25 18:25:33 -0500

    Terminada la parte de backend, empieza el CSS

--- JS/takepon.js
+++ JS/takepon.js
@@ -1,5 +1,12 @@
 let ataqueJugador
 let ataEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
+
+let seccionSeleccionarAtaque = document.getElementById("selAtaque")
+seccionSeleccionarAtaque.style.display = "none";
+let seccionReiniciar = document.getElementById("reiniciar")
+seccionReiniciar.style.display = "none"
 let botonMascotaJugador = document.getElementById("boton-mascota");
 botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 let botonFuego = document.getElementById("boton-fuego")
@@ -8,8 +15,13 @@ let botonAgua = document.getElementById("boton-agua")
 botonAgua.addEventListener('click', ataqueAgua)
 let botonTierra = document.getElementById("boton-tierra")
 botonTierra.addEventListener('click', ataqueTierra)
+let botonReiniciar = document.getElementById("boton-reiniciar")
+botonReiniciar.addEventListener('click', reiniciarJuego)
 
 function seleccionarMascotaJugador(){
+    seccionSeleccionarAtaque.style.display = "block"
+    let seccionSeleccionarMascota = document.getElementById("selMascota")
+    seccionSeleccionarMascota.style.display = "none"
     let inputWailzemonk = document.getElementById("Wailmezonk");
     let inputSkiploont = document.getElementById("Skiploont");
     let inputFennecros = document.getElementById("Fennecros");
@@ -50,30 +62,82 @@ function aleatorio(min,max){
 
 function ataqueAgua(){
   ataqueJugador = "AGUA"
-  alert("Atacaste con AGUA")
   ataqueEnemigo()
 }
 function ataqueFuego(){
   ataqueJugador = "FUEGO"
-  alert("Atacaste con FUEGO")
   ataqueEnemigo()
 }
 function ataqueTierra(){
   ataqueJugador = "TIERRA"
-  alert("Atacaste con Tierra")
   ataqueEnemigo()
 }
 function ataqueEnemigo(){
   let ataqueAleatorio = aleatorio(1,3)
   if(ataqueAleatorio == 1){
     ataEnemigo = "FUEGO"
-    alert("El enemigo ataco con FUEGO")
   } else if (ataqueAleatorio == 2){
     ataEnemigo = "AGUA"
-    alert("El enemigo ataco con AGUA")
   } else if(ataqueAleatorio == 3){
     ataEnemigo = "TIERRA"
-    alert("El enemigo ataco con TIERRA")
   }
-  
+  combate()
+}
+
+function combate(){
+  let spanVidasJugador = document.getElementById("vidas-jugador")
+  let spanVidasEnemigo = document.getElementById("vidas-enemigo")
+  if (ataqueJugador == ataEnemigo) {
+    crearMensaje("EMPATE")
+  } else if (ataqueJugador == "FUEGO" && ataEnemigo == "TIERRA") {
+      crearMensaje("GANASTE")
+      vidasEnemigo--
+      spanVidasEnemigo.innerHTML = vidasEnemigo
+  } else if (ataqueJugador == "AGUA" && ataEnemigo == "FUEGO") {
+      crearMensaje("GANASTE")
+      vidasEnemigo--
+      spanVidasEnemigo.innerHTML = vidasEnemigo
+  } else if (ataqueJugador == "TIERRA" && ataEnemigo == "AGUA") {
+      crearMensaje("GANASTE")
+      vidasEnemigo--
+      spanVidasEnemigo.innerHTML = vidasEnemigo
+  } else {
+      crearMensaje("PERDISTE")
+      vidasJugador--;
+      spanVidasJugador.innerHTML = vidasJugador
+  } 
+  revisarVidas()
+}
+
+function revisarVidas(){
+  if(vidasEnemigo == 0){
+    crearMensajeFinal("FELICIDADES ERES UN MAESTRO TAKEPON")
+  }else if (vidasJugador == 0){
+    crearMensajeFinal("ENTRENA MAS!!!!")
+  }
+}
+
+function crearMensaje(resultado){
+  let seccionMensajes = document.getElementById("mensajes")
+  let parrafo = document.createElement("p")
+  parrafo.innerHTML = "Tu takepon ataco con " + ataqueJugador  + ", el takepon del enemigo ataco con " + ataEnemigo + " " + resultado;
+  seccionMensajes.appendChild(parrafo)
+}
+
+function crearMensajeFinal(resultadoFinal){
+  let seccionMensajes = document.getElementById("mensajes")
+  let parrafo = document.createElement("p")
+  parrafo.innerHTML = resultadoFinal
+  seccionMensajes.appendChild(parrafo)
+  let botonFuego = document.getElementById("boton-fuego")
+  botonFuego.disabled = true
+  let botonAgua = document.getElementById("boton-agua")
+  botonAgua.disabled = true
+  let botonTierra = document.getElementById("boton-tierra")
+  botonTierra.disabled = true
+  seccionReiniciar.style.display = "block"
+}
+
+function reiniciarJuego(){
+  location.reload()
 }
