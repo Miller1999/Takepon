0cc4e2229668750940c55e2583e003bc65af9f7e
Miller1999 <millerjavierarias@gmail.com>
2023-01-28 12:47:52 -0500

    Terminada la parte del mapa

--- JS/takepon.js
+++ JS/takepon.js
@@ -22,6 +22,8 @@ const ataquesdelEnemigo = document.getElementById("ataquedelEnemigo")
 //let botonTierra = document.getElementById("boton-tierra")
 const contenedorTarjetas = document.getElementById("contenedor-tarjetas")
 const contenedorAtaques = document.getElementById("contenedorAtaques")
+const seccionVerMapa = document.getElementById("verMapa")
+const mapa = document.getElementById("mapa")
 
 let takepones = []
 let ataqueJugador = []
@@ -31,6 +33,7 @@ let inputWailzemonk
 let inputSkiploont
 let inputFennecros
 let mascotaJugador
+let mascotaJugadorObjeto
 let ataquesTakepon
 let ataquesTakeponEnemigo
 let botonFuego
@@ -43,19 +46,52 @@ let victoriasJugador = 0
 let victoriasEnemigo = 0
 let vidasJugador = 3
 let vidasEnemigo = 3
+let lienzo = mapa.getContext("2d")
+let intervalo
+let mapaBackground = new Image()
+mapaBackground.src = "./Imagenes/mokemap.png"
+let alturaBuscada
+let anchoMapa = window.innerWidth - 20
+alturaBuscada = anchoMapa * 600 / 800
+mapa.width = anchoMapa  
+mapa.height = alturaBuscada
+const anchoMaximo = 350
+
+if(anchoMapa > anchoMaximo){
+  anchoMapa = anchoMaximo - 20
+}
 
 class Takepon{
-  constructor(nombre,foto,vida){
+  constructor(nombre,foto,vida,x=10,y=10){
     this.nombre = nombre
     this.foto = foto
     this.vida = vida
     this.ataques = []
+    this.ancho = 40
+    this.alto = 40
+    this.x = aleatorio(0, mapa.width-this.ancho)
+    this.y = aleatorio(0, mapa.height-this.alto)
+    this.mapaFoto = new Image()
+    this.mapaFoto.src = foto
+    this.velocidadX = 0
+    this.velocidadY = 0
+  }
+  pintarMokepon(){
+    lienzo.drawImage(
+      this.mapaFoto,
+      this.x,
+      this.y,
+      this.ancho,
+      this.alto)
   }
 }
 
 let wailmezonk = new Takepon("Wailzemonk",'./Imagenes/Wailmezonk.jpg', 5)
 let skiploont = new Takepon("Skiploont",'./Imagenes/Skiploont.jpg',5)
 let fennecros = new Takepon("Fennecros",'./Imagenes/Fennecros.jpg',5)
+let wailmezonkEnemigo = new Takepon("Wailzemonk",'./Imagenes/Wailmezonk.jpg', 5)
+let skiploontEnemigo = new Takepon("Skiploont",'./Imagenes/Skiploont.jpg',5,)
+let fennecrosEnemigo = new Takepon("Fennecros",'./Imagenes/Fennecros.jpg',5)
 
 wailmezonk.ataques.push(
   {nombre: "💧", id: "boton-agua"},
@@ -79,10 +115,33 @@ fennecros.ataques.push(
   {nombre: "🌱", id: "boton-tierra"}
 )
 
+wailmezonkEnemigo.ataques.push(
+  {nombre: "💧", id: "boton-agua"},
+  {nombre: "💧", id: "boton-agua"},
+  {nombre: "💧", id: "boton-agua"},
+  {nombre: "🔥", id: "boton-fuego"},
+  {nombre: "🌱", id: "boton-tierra"}
+)
+skiploontEnemigo.ataques.push(
+  {nombre: "💧", id: "boton-agua"},
+  {nombre: "🔥", id: "boton-fuego"},
+  {nombre: "🌱", id: "boton-tierra"},
+  {nombre: "🌱", id: "boton-tierra"},
+  {nombre: "🌱", id: "boton-tierra"}
+)
+fennecrosEnemigo.ataques.push(
+  {nombre: "💧", id: "boton-agua"},
+  {nombre: "🔥", id: "boton-fuego"},
+  {nombre: "🔥", id: "boton-fuego"},
+  {nombre: "🔥", id: "boton-fuego"},
+  {nombre: "🌱", id: "boton-tierra"}
+)
+
 takepones.push(wailmezonk,skiploont,fennecros)
 
 seccionSeleccionarAtaque.style.display = "none";
 seccionReiniciar.style.display = "none"
+seccionVerMapa.style.display = "none"
 botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 botonReiniciar.addEventListener('click', reiniciarJuego)
 
@@ -102,8 +161,11 @@ takepones.forEach((takepon) => {
 })
 
 function seleccionarMascotaJugador(){
-    seccionSeleccionarAtaque.style.display = "flex"
+    
     seccionSeleccionarMascota.style.display = "none"
+    seccionVerMapa.style.display = "flex"
+    
+
     if(inputWailzemonk.checked == true)
     {
         spanMascotaJugador.innerHTML = inputWailzemonk.id
@@ -123,7 +185,8 @@ function seleccionarMascotaJugador(){
         alert("No escogiste ningun takepon")
     }
     extraerAtaques(mascotaJugador)
-    seleccionarMascotaEnemigo()
+    iniciarMapa()
+    
 }
 
 function extraerAtaques(mascotaJugador){
@@ -172,10 +235,9 @@ function secuenciaAtaque(){
   
 }
 
-function seleccionarMascotaEnemigo(){
-  let takeponEnemigo = aleatorio(0,takepones.length-1);
-  spanMascotaEnemigo.innerHTML = takepones[takeponEnemigo].nombre
-  ataquesTakeponEnemigo = takepones[takeponEnemigo].ataques
+function seleccionarMascotaEnemigo(enemigo){
+  spanMascotaEnemigo.innerHTML = enemigo.nombre
+  ataquesTakeponEnemigo = enemigo.ataques
   secuenciaAtaque()
 }
 
@@ -268,4 +330,95 @@ function aleatorio(min,max){
 
 function reiniciarJuego(){
   location.reload()
+}
+
+function pintarCanvas(){
+  mascotaJugadorObjeto.x = mascotaJugadorObjeto.x + mascotaJugadorObjeto.velocidadX
+  mascotaJugadorObjeto.y = mascotaJugadorObjeto.y + mascotaJugadorObjeto.velocidadY
+  lienzo.clearRect(0,0,mapa.width,mapa.height)
+  lienzo.drawImage(mapaBackground,0,0,mapa.width,mapa.height)
+  mascotaJugadorObjeto.pintarMokepon()
+  wailmezonkEnemigo.pintarMokepon()
+  skiploontEnemigo.pintarMokepon()
+  fennecrosEnemigo.pintarMokepon()
+  if(mascotaJugadorObjeto.velocidadX != 0 || mascotaJugadorObjeto.velocidadY != 0){
+    revisarColision(wailmezonkEnemigo)
+    revisarColision(skiploontEnemigo)
+    revisarColision(fennecrosEnemigo)
+  }
+}
+
+function moverDerecha(){
+  mascotaJugadorObjeto.velocidadX = 5
+}
+function moverIzquierda(){
+  mascotaJugadorObjeto.velocidadX = -5
+}
+function moverArriba(){
+  mascotaJugadorObjeto.velocidadY = -5
+}
+function moverAbajo(){
+  mascotaJugadorObjeto.velocidadY = 5
+}
+function detenerMovimiento(){
+  mascotaJugadorObjeto.velocidadX = 0
+  mascotaJugadorObjeto.velocidadY = 0
+}
+function sePresionoUnaTecla(event){
+  switch(event.key){
+    case 'ArrowUp':
+      moverArriba()
+      break
+    case 'ArrowDown':
+      moverAbajo()
+      break
+    case 'ArrowLeft':
+      moverIzquierda()
+      break
+    case 'ArrowRight':
+      moverDerecha()
+      break
+    default:
+      break
+  }
+}
+function iniciarMapa(){
+
+  mascotaJugadorObjeto = obtenerMascota(mascotaJugador)
+  intervalo = setInterval(pintarCanvas,50)
+  window.addEventListener('keydown', sePresionoUnaTecla)
+  window.addEventListener('keyup', detenerMovimiento)
+}
+
+function obtenerMascota(){
+  for(let i = 0;i<takepones.length;i++){
+    if(mascotaJugador === takepones[i].nombre){
+      return takepones[i]
+    }
+  }
+}
+
+function revisarColision(enemigo){
+  const arribaEnemigo = enemigo.y
+  const abajoEnemigo = enemigo.y + enemigo.alto
+  const derechaEnemigo = enemigo.x + enemigo.ancho
+  const izquierdaEnemigo = enemigo.x
+
+  const arribaMascota = mascotaJugadorObjeto.y
+  const abajoMascota = mascotaJugadorObjeto.y + mascotaJugadorObjeto.alto
+  const derechaMascota = mascotaJugadorObjeto.x + mascotaJugadorObjeto.ancho
+  const izquierdaMascota = mascotaJugadorObjeto.x
+  if(
+    abajoMascota < arribaEnemigo ||
+    arribaMascota > abajoEnemigo ||
+    derechaMascota < izquierdaEnemigo ||
+    izquierdaMascota > derechaEnemigo
+  ){
+    return;
+  }
+  detenerMovimiento()
+  clearInterval(intervalo)
+  seccionSeleccionarAtaque.style.display = "flex"
+  seccionVerMapa.style.display = "none"
+  seleccionarMascotaEnemigo(enemigo)
 }
